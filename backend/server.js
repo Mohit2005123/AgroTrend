1f09048600ea8d88de0b7a7771e724b2299bb17f
Mohit2005123 <mohitmongia2005@gmail.com>
2024-08-11 20:02:33 +0530

    Coded backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,21 @@ import authRoutes from './routes/auth.routes.js';
 import connectToMongoDb from './db/connectMongo.js';
 import newsRoutes from './routes/news.routes.js';
 import marketRoutes from './routes/market.routes.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 const app= express();
-
+const __dirname= path.resolve();
 const PORT= process.env.PORT || 3000;
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/news', newsRoutes );
 app.use('/api/marketdata', marketRoutes);
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
 app.listen(PORT, ()=>{
     connectToMongoDb();
     console.log(`Server is running on port ${PORT}`);
